23b7cbbaa2907ec7389ecccc5284e2e6bc8ebacf
tsukamoto0912 <186455239+tsukamoto0912@users.noreply.github.com>
2025-01-20 16:34:25 +0900

    削除機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,12 @@ class PostsController < ApplicationController
     redirect_to post_path(post.id)
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:location, :text)
